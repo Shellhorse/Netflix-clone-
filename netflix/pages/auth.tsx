cc5ca88656141dfe2ffb80af3c4abad7355c086b
Shellhorse <jo.shellhorse@gmail.com>
2023-04-17 17:51:53 -0400

    Added github Oath secure routes

--- netflix/pages/auth.tsx
+++ netflix/pages/auth.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { signIn } from 'next-auth/react'
 import { useRouter } from "next/router";
+import { FaGithub } from 'react-icons/fa'
 
 
 const Auth = () => {
@@ -23,15 +24,12 @@ const Auth = () => {
             await signIn('credentials', {
                 email,
                 password,
-                redirect: false,
-                callbackUrl: '/'
+                callbackUrl: '/profiles'
             });
-
-            router.push('/')
         } catch (error) {
             console.log(error);
         }
-    }, [email, password, router])
+    }, [email, password])
 
     const register = useCallback(async () => {
         try {
@@ -67,6 +65,11 @@ const Auth = () => {
                             <Input label="Password" value={password} onChange={(ev : any) => setPassword(ev.target.value)} type="password" id="password"/>
                         </div>
                         <button  onClick={variant == 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant == 'login' ? 'Login' : 'Sign up'}</button>
+                        <div onClick={() => signIn('github', {callbackUrl: '/profiles'})} className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacit-80 transition">
+                                <FaGithub size = {30}></FaGithub>
+                            </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
                                 {variant == 'login' ? 'First time using Netflix?' : 'Already have an account?'}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
--- netflix/pages/index.tsx
+++ netflix/pages/index.tsx
@@ -1,7 +1,33 @@
-import { Inter } from 'next/font/google'
+import useCurrentUser from '@/hooks/userCurrentUser'
+import { NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
 
-const inter = Inter({ subsets: ['latin'] })
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context)
+
+    if(!session) {
+        return { 
+            redirect: {
+                destination: '/auth',
+                permanant: false
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
+}
 
 export default function Home() {
- return <h1>Nexflix Clone</h1>
+const { data: user } = useCurrentUser();
+
+
+ return (
+    <>
+        <h1>Nexflix Clone</h1>
+        <p className='text-white'>Logged in as {user?.email}</p>
+        <button className='h-10 w-full bg-white' onClick = {() => signOut()}>Logout</button>
+    </>
+ )
 }
